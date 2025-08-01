e5f6de68f91ef5d3de3dc79072a78794a01b83f9
Aditya <adityasainics10@gmail.com>
2025-08-01 15:33:17 +0530

    rendering comments by status

--- client/src/components/DisplayComments.tsx
+++ client/src/components/DisplayComments.tsx
@@ -1,16 +1,32 @@
 interface Comment {
   id: string;
   content: string;
+  status: string;
 }
 
 export default function DisplayComments({ comments }: {
   comments: Comment[];
 }) {
-  
+
   const renderComments = comments.map((comment: Comment) => {
+    let content ;
+
+    if(comment.status === 'approved'){
+      content = comment.content;
+    }
+
+    if(comment.status === 'pending'){
+      content = "COMMENT WAITING FOR APPROVAL"
+    }
+
+    if(comment.status === 'rejected'){
+      content = "COMMENT IS REJECTED"
+
+    }
+
     return (
       <li key={comment.id}>
-        {comment.content}
+        {content}
       </li>
     );
   });
